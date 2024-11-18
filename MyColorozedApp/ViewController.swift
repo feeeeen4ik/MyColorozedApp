dac0d95d54cd08b5b4857efad5adee37b6fb5f83
feeeeeen4ik <feeeeen4ik1994@gmail.com>
2024-11-18 21:01:08 +0500

    Initial Commit

---
+++ MyColorozedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyColorozedApp
+//
+//  Created by Феликс Антонович on 18.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
